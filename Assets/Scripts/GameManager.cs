b44a39b471afda92d1b8ba47931a31298140a53f
keiserjon02 <joneduardom@gmail.com>
2024-04-09 14:23:52 +0200

    Imported Generator + Made ScirptableObjects and ResourceBank

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    
+    public ResourceBankScriptableObject ResourceBank;
+    public float energy;
+
+    private void Awake()
+    {
+        instance = this;
+        energy = ResourceBank.energy;
+        if (energy == 0)
+        {
+            energy = 100;
+        }
+    }
+}
