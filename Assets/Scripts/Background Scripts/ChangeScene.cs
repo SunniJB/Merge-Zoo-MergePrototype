c22bb366d37013d025308bcb5ef64717ff091387
TorFDahl <89240132+TorFDahl@users.noreply.github.com>
2024-04-10 12:11:50 +0200

    Added a way to see that a new scene is being loaded
    It just puts a panel on screen that says "loading..."

--- Assets/Scripts/Background Scripts/ChangeScene.cs
+++ Assets/Scripts/Background Scripts/ChangeScene.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
 
-
+    [SerializeField] private Image loadingPanel;
+    [SerializeField] private Text loadingText;
     void Awake()
     {
-
+        loadingPanel.gameObject.SetActive(false);
+        SceneManager.activeSceneChanged += HideLoadingPanel;
     }
 
     // Update is called once per frame
@@ -22,5 +25,12 @@ public class ChangeScene : MonoBehaviour
     public void LoadSceneAsync(int sceneIndex)
     {
         SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
+        loadingPanel.gameObject.SetActive(true);
+        
+    }
+
+    private void HideLoadingPanel(Scene prevScene, Scene curScene)
+    {
+        loadingPanel.gameObject.SetActive(false);
     }
 }
