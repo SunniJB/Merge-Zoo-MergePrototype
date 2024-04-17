2c7233cad43a4c4c31179703f9fe68c33af558d4
SunniJB <nepimpeta@gmail.com>
2024-04-17 13:10:18 +0200

    Added friends list

--- Assets/Scripts/Friendslist/SceneSwitcher.cs
+++ Assets/Scripts/Friendslist/SceneSwitcher.cs
@@ -15,6 +15,6 @@ public static class SceneSwitcher
     public static void LeaveFriend()
     {
         currentFriend = null;
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 }
