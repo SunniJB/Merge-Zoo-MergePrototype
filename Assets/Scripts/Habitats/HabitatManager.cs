12e09c56c3caab80f95864712dd39ae125db6ced
keiserjon02 <joneduardom@gmail.com>
2024-05-07 14:18:13 +0200

    Updated ZooOverworld

--- Assets/Scripts/Habitats/HabitatManager.cs
+++ Assets/Scripts/Habitats/HabitatManager.cs
@@ -1,12 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 //using Unity.VisualScripting;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class HabitatManager : MonoBehaviour
 {
     public Habitat[] habitat;
     public GameObject animal;
+
+    private void Start()
+    {
+        Inhabit();
+    }
 
     public void Inhabit()
     {
@@ -14,7 +21,7 @@ public class HabitatManager : MonoBehaviour
         {
             if (Habitat.occupiedBy == null)
             {
-                Object spawnedAnimal = Instantiate(Habitat, Habitat.gameObject.transform.position, Habitat.gameObject.transform.rotation);
+                Instantiate(Habitat, Habitat.gameObject.transform.position, Habitat.gameObject.transform.rotation);
 
             }
         }
