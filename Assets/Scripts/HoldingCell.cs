e764c2369fd1f8979d93699158902c1d93509b28
tusjmod <64898919+tusjmod@users.noreply.github.com>
2024-05-08 13:18:53 +0200

    Update HoldingCell.cs

--- Assets/Scripts/HoldingCell.cs
+++ Assets/Scripts/HoldingCell.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using UnityEditor.iOS;
 using UnityEngine;
 
 public class HoldingCell : MonoBehaviour
